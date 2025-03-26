1d062e5b5f6dbeec885493e9cf5a2faf91412bf9
mrsinashi <ignatiusferum@gmail.com>
2025-03-26 19:31:14 +0800

    Corrected Design

--- configs/.config/polybar/scripts/menu.sh
+++ configs/.config/polybar/scripts/menu.sh
@@ -3,7 +3,7 @@ CURSOR_POS=$(xdotool getmouselocation --shell)
 eval "$CURSOR_POS"
 if [ "$X" -ge "0" ] && [ "$X" -le "53" ] && \
    [ "$Y" -ge "1040" ] && [ "$Y" -le "1080" ]; then
-    echo "%{B#2f2f2f}%{F#2dcdcd}      %{B-}%{F-}"
+    echo "%{B#373B41}%{F#54D5DE}      %{B-}%{F-}"
 else
-    echo "%{B#000000}%{F#ffffff}      %{B-}%{F-}"
+    echo "%{B#000000}%{F#FFFFFF}      %{B-}%{F-}"
 fi
